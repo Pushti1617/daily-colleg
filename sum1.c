63ab7b55a367de983201575879942f03d1fe2c05
Pushti1617 <pushti1617@gmail.com>
2025-07-04 00:33:22 +0530

    Create sum1.c

---
+++ sum1.c
@@ -0,0 +1,12 @@
+vioid main()
+{
+  int a,b;
+  clrscr();
+  printf("Enter the value of A: ");
+  scanf("%d",&a);
+  printf("Enter the value of B: ");
+  scanf("%d",&b);
+  printd("The sum of this no. is: %d",a+b);
+  getch();
+  return 0;
+}
